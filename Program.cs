0fc82453c1e3ed1856bba91d217b1e5d0c002ef8
Ellesent <fpaterin@uccs.edu>
2018-09-11 19:15:59 -0700

    modifying url used

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace guac_storage
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .UseUrls("http://*:5000/");
     }
 }
